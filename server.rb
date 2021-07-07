6f23061c02e104dfe692c390592128b0341432aa
Ángel García <angel.garciafernandez@hotmail.com>
2021-07-07 09:40:01 +0200

    Improve server

--- server.rb
+++ server.rb
@@ -4,25 +4,27 @@ require 'yaml/store'
 Dir['resources/*.rb'].each { |file| require_relative file }
 require_relative 'config/routes'
 
-port = 3000
+port = 9999
 server = TCPServer.new(port)
 
 #Create a YAML Store
 store = YAML::Store.new('persistance/data.yml')
 
 
-loop do
-  client = server.accept
+while session = server.accept
+  request = session.gets
 
-  request_line = client.readline
-  method_token, target, version_number = request_line.split
-  puts "Received a #{method_token} request to #{target} with #{version_number}"
+  unless request.nil?
+    method_token, target, version_number = request.split
+    puts "Received a #{method_token} request to #{target} with #{version_number}"
 
-  routes = Routes.new
+    # Route request & build response
+    routes = Routes.new
+    http_response = routes.resolve(method_token, target)
 
-  http_response = routes.resolve(method_token, target)
-
-  client.puts http_response
-  client.close
+    # Send response
+    session.puts http_response
+  end
+  session.close
 end
 
