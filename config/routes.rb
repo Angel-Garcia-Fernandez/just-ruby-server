4aacd930a5e8ab59bcd2cd07cbb8ae756e5365cb
chloe <chloe@meisterlabs.com>
2021-07-08 14:48:55 +0200

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,11 @@ class Routes < BaseRoutes
     get '/books/:isbn/edit', to: 'books#edit'
     put '/books/:isbn', to: 'books#update'
     delete '/books/:isbn', to: 'books#destroy'
-    get '/books/:isbn/update', to: 'books_info#find_book'
+    get '/books/:isbn/update', to: 'book_info#find_book'
 
     get '/writings', redirect_to: '/books'
 
     get '*', to: 'errors#not_found'
+    get '/error', to: 'errors#error'
   end
 end
